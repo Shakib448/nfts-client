ec300d539569621c3506768530b764b9a1ad35b2
Shakiba448 <shakiba448@gmail.com>
2021-03-10 08:37:08 +0600

    WIP on HOME

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import NavigationBar from "./Shared/NavigationBar";
 import Theme from "./Theme/Theme";
 import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 import GlobalStyles from "./Theme/GlobalStyle";
+import "../src/Shared/MobileBar/styles.css";
+import { Example } from "./Shared/MobileBar/Example";
 
 function App() {
   return (
     <MuiThemeProvider theme={Theme}>
       <CssBaseline />
       <GlobalStyles />
-      <NavigationBar />
+      {/* <NavigationBar /> */}
+      <Example />
     </MuiThemeProvider>
   );
 }
