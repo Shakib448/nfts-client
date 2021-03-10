710b3fa7bd0690e2b2a8ace8776b63cbb61f15aa
Shakiba448 <shakiba448@gmail.com>
2021-03-10 19:25:15 +0600

    WIP on HOME

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Theme from "./Theme/Theme";
 import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 import GlobalStyles from "./Theme/GlobalStyle";
 import { MobileBar } from "./Shared/MobileBar/MobileBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <MuiThemeProvider theme={Theme}>
       <CssBaseline />
       <GlobalStyles />
-      <NavigationBar />
-      <MobileBar />
+      <Router>
+        <NavigationBar />
+        <MobileBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
     </MuiThemeProvider>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,8 @@
+import { Box } from "@material-ui/core";
+import React from "react";
+
+const Home = () => {
+  return <Box variant="div"></Box>;
+};
+
+export default Home;
