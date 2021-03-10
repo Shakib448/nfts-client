faf77b56afb1fc1ed1a5e82dded0a0e5220d1a45
Shakiba448 <shakiba448@gmail.com>
2021-03-10 07:05:46 +0600

    WIP on HOME

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.scss";
 import NavigationBar from "./Components/NavigationBar";
+import Theme from "./Theme/Theme";
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
+import GlobalStyles from "./Theme/GlobalStyle";
 
 function App() {
   return (
-    <>
+    <MuiThemeProvider theme={Theme}>
+      <CssBaseline />
+      <GlobalStyles />
       <NavigationBar />
-    </>
+    </MuiThemeProvider>
   );
 }
 
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -4,42 +4,68 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Box, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(4),
+    fontSize: 20,
+  },
+  appbar: {
+    display: "flex",
+    justifyContent: "space-between",
   },
   title: {
     flexGrow: 1,
   },
+  toolBar: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
 }));
 
-export default function NavigationBar() {
+const NavigationBar = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+      <AppBar position="fixed" color="transparent" className={classes.appBar}>
+        <Toolbar className={classes.toolBar}>
+          <Grid container>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+          </Grid>
+
+          <Grid container>
+            <Button className={classes.menuButton} color="inherit">
+              Home
+            </Button>
+            <Button className={classes.menuButton} color="inherit">
+              About
+            </Button>
+            <Button className={classes.menuButton} color="inherit">
+              Contact
+            </Button>
+          </Grid>
+
+          <Grid container justify="flex-end">
+            <Button className={classes.menuButton} color="inherit">
+              Login
+            </Button>
+            <Button className={classes.menuButton} color="inherit">
+              Signup
+            </Button>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
   );
-}
+};
+
+export default NavigationBar;
