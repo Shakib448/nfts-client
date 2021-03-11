7d6421b0bd891325edc2f10292461373b2f4d965
Shakiba448 <shakiba448@gmail.com>
2021-03-11 13:12:21 +0600

    WIP on Animation

--- src/Components/Home/HomeAnimation.js
+++ src/Components/Home/HomeAnimation.js
@@ -1,18 +1,20 @@
 import { Box, makeStyles, Grid } from "@material-ui/core";
 import React from "react";
 import clsx from "clsx";
-import img1 from "../../Images/Primary button-1.png";
-import img2 from "../../Images/Primary button-2.png";
-import img3 from "../../Images/Primary button-3.png";
-import img4 from "../../Images/Primary button-4.png";
-import img5 from "../../Images/Primary button-5.png";
-import img6 from "../../Images/Primary button-6.png";
-import img7 from "../../Images/Primary button.png";
+import img from "../../Images/robot.png";
 import "./Home.scss";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: "relative",
+    flexGrow: 1,
+    padding: theme.spacing(4),
+    [theme.breakpoints.down("md")]: {
+      marginTop: "100px",
+    },
+  },
+  img: {
+    width: "100%",
+    height: "auto",
   },
 }));
 
@@ -21,52 +23,12 @@ const HomeAnimation = () => {
   return (
     <>
       <Box variant="div" className={clsx(classes.root)}>
-        {/* {animImage.map((item, index) => ( */}
-        {/* <Grid item key={index} className={item.class}>
-            <img src={item.img} alt="primary" />
-          </Grid> */}
-        {/* ))} */}
+        <Grid item>
+          <img src={img} alt="primary" className={clsx(classes.img)} />
+        </Grid>
       </Box>
     </>
   );
 };
-
-const animImage = [
-  {
-    img: img1,
-    alt: "primary button 1",
-    class: "img1",
-  },
-  {
-    img: img2,
-    alt: "primary button 2",
-    class: "img2",
-  },
-  {
-    img: img3,
-    alt: "primary button 3",
-    class: "img3",
-  },
-  {
-    img: img4,
-    alt: "primary button 4",
-    class: "img4",
-  },
-  {
-    img: img5,
-    alt: "primary button 5",
-    class: "img5",
-  },
-  {
-    img: img6,
-    alt: "primary button 1",
-    class: "img6",
-  },
-  {
-    img: img7,
-    alt: "primary button 7",
-    class: "img7",
-  },
-];
 
 export default HomeAnimation;
