38f0b847002a95689f9daa6ba2132bee20dbbdeb
Shakiba448 <shakiba448@gmail.com>
2021-03-11 11:04:29 +0600

    WIP on Animation

--- src/Components/Home/HomeAnimation.js
+++ src/Components/Home/HomeAnimation.js
@@ -9,19 +9,64 @@ import img5 from "../../Images/Primary button-5.png";
 import img6 from "../../Images/Primary button-6.png";
 import img7 from "../../Images/Primary button.png";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    position: "relative",
+  },
+}));
 
 const HomeAnimation = () => {
   const classes = useStyles();
   return (
     <>
-      <Box variant="div">
-        <Grid item>
-          <img src={img1} alt="primary" />
-        </Grid>
+      <Box variant="div" className={clsx(classes.root)}>
+        {animImage.map((item, index) => (
+          <Grid item key={index}>
+            <img src={item.img} alt="primary" className={item.class} />
+          </Grid>
+        ))}
       </Box>
     </>
   );
 };
+
+const animImage = [
+  {
+    img: img1,
+    alt: "primary button 1",
+    class: "img1",
+  },
+  {
+    img: img2,
+    alt: "primary button 2",
+    class: "img2",
+  },
+  {
+    img: img3,
+    alt: "primary button 3",
+    class: "img3",
+  },
+  {
+    img: img4,
+    alt: "primary button 4",
+    class: "img4",
+  },
+  {
+    img: img5,
+    alt: "primary button 5",
+    class: "img5",
+  },
+  {
+    img: img6,
+    alt: "primary button 1",
+    class: "img6",
+  },
+  {
+    img: img7,
+    alt: "primary button 7",
+    class: "img7",
+  },
+];
 
 export default HomeAnimation;
