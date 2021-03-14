0679ba0520b71326f2e62d9f17bd4428d4e1a077
Shakiba448 <shakiba448@gmail.com>
2021-03-14 15:41:31 +0600

    WIP on Dashboard

--- src/Components/Dashboard/DashNav/Sidebar.js
+++ src/Components/Dashboard/DashNav/Sidebar.js
@@ -1,8 +1,44 @@
-import { Box } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  //   makeStyles,
+} from "@material-ui/core";
+// import clsx from "clsx";
 import React from "react";
 
+// const useStyles = makeStyles((theme) => ({
+//   dashNav: {
+//     height: "100vh",
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//   },
+// }));
+
 const Sidebar = () => {
-  return <Box></Box>;
+  //   const classes = useStyles();
+  return (
+    <List component="nav" aria-label="main mailbox folders">
+      <ListItem button>
+        <ListItemIcon></ListItemIcon>
+        <ListItemText primary="Connect Wallet" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon></ListItemIcon>
+        <ListItemText primary="Collections" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon></ListItemIcon>
+        <ListItemText primary="Featured" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon></ListItemIcon>
+        <ListItemText primary="Profile" />
+      </ListItem>
+    </List>
+  );
 };
 
 export default Sidebar;
--- src/Components/Dashboard/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard/Dashboard.js
@@ -1,10 +1,23 @@
-import { Grid } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
+import clsx from "clsx";
 import React from "react";
+import Sidebar from "../DashNav/Sidebar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
   return (
     <>
-      <Grid item md={3} lg={3} sm={12}></Grid>
+      <Grid item md={3} lg={3} sm={12} className={clsx(classes.root)}>
+        <Grid container justify="center" alignItems="center">
+          <Sidebar />
+        </Grid>
+      </Grid>
       <Grid item md={9} lg={9} sm={12}></Grid>
     </>
   );
